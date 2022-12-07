6b0b90bd6ab221554876a9203f8d8b13b94a83e3
Mateo Kladarić <52569856+MrFacysus@users.noreply.github.com>
2022-12-07 23:40:15 +0100

    Add files via upload

---
+++ ShutDownTimer/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShutDownTimer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Get the time to shut down
+            Console.WriteLine("Enter the time to shut down (hh:mm:ss): (19:20:55)");
+            string time = Console.ReadLine();
+            // Convert time to DateTime
+            DateTime timeToShutDown = DateTime.Parse(time);
+            // Get the current time
+            DateTime currentTime = DateTime.Now;
+            // Calculate the time to wait
+            TimeSpan timeToWait = timeToShutDown - currentTime;
+            // Wait for the time to wait
+            System.Threading.Thread.Sleep(timeToWait);
+            // Shut down the computer
+            System.Diagnostics.Process.Start("shutdown", "-s -t 0");
+        }
+    }
+}
